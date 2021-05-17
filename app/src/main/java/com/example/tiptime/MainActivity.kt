4a5da2aa0885b1754ee39c24eea8d3be6dcc58d6
Derlys <derlysr6@gmail.com>
2021-05-17 05:54:25 -0300

    add function to hiden a keyboard after click in KEYCODE_ENRTER

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.tiptime
 
+import android.content.Context
 import android.icu.text.NumberFormat
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.annotation.RequiresApi
 import com.example.tiptime.databinding.ActivityMainBinding
 import kotlin.math.ceil
@@ -19,7 +23,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         //set a function calculateTip() in the UI element button @+id/calculate
         binding.calculate.setOnClickListener { calculateTip() }
-
+        //set a function calculateTip() in the UI element EditText @+id/costOfServiceEditText
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(
+                view,
+                keyCode
+            )
+        }
     }
 
     @RequiresApi(Build.VERSION_CODES.N)
@@ -46,6 +56,21 @@ class MainActivity : AppCompatActivity() {
         }
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipamount.text = getString(R.string.tip_amount, formattedTip)
-
     }
+
+    /*
+    The handleKeyEvent() is a private helper function that hides the onscreen
+    keyboard if the keyCode input parameter is equal to KeyEvent.KEYCODE_ENTER
+     */
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+
 }
